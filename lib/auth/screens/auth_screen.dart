66409e115005a03faeb850f5448d5ffedc76d7e6
Fagner Carvalho <fagnerlc@outlook.com>
2020-08-13 20:43:02 -0300

    Correção de bug login google alteração de layout da tela login

--- lib/auth/screens/auth_screen.dart
+++ lib/auth/screens/auth_screen.dart
@@ -12,71 +12,80 @@ class AuthScreeen extends StatelessWidget {
     FirebaseService firebaseService = FirebaseService();
 
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Colors.teal,
-                  Colors.black12,
-                  Colors.teal,
-                ],
+      body: Container(
+        decoration: BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage('assets/images/equip_comp_01.png'), fit: BoxFit.cover
+      )
+    ),
+        child: Stack(
+          children: [
+            //Image.asset('assets/images/equip_comp_01.png',fit: BoxFit.cover,),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    //Colors.teal,
+                    Colors.black12,
+                    Colors.teal,
+                    Colors.black12,
+                  ],
+                ),
               ),
             ),
-          ),
-          Container(
-            width: double.infinity,
-            child: SingleChildScrollView(
-              padding: EdgeInsets.only(top: 140),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(bottom: 30),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.black45,
-                      boxShadow: [
-                        BoxShadow(
-                            blurRadius: 8,
-                            color: Colors.black26,
-                            offset: Offset(0, 7)),
-                      ],
+            Container(
+              width: double.infinity,
+              child: SingleChildScrollView(
+                padding: EdgeInsets.only(top: 180),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 30),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.black45,
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 8,
+                              color: Colors.black26,
+                              offset: Offset(0, 7)),
+                        ],
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        vertical: 7,
+                        horizontal: 70,
+                      ),
+                      transform: Matrix4.rotationZ(-8 * pi / 180)
+                        ..translate(-10.0),
+                      child: Text(
+                        'NoFSD',
+                        style: TextStyle(
+                            color: Colors.white70,
+                                //Theme.of(context).accentTextTheme.headline6.color,
+                            fontSize: 45,
+                            fontFamily: 'Anton'),
+                      ),
                     ),
-                    padding: EdgeInsets.symmetric(
-                      vertical: 7,
-                      horizontal: 70,
+                    AuthCard(),
+                    GoogleSignInButton(
+                      onPressed: () async {
+                        bool f;
+                        await firebaseService.handleSignIn();
+                        f = firebaseService.usuario.toString() != null;
+                        if (f) {
+                          Navigator.of(context).pushNamed(AppRotas.HOME);
+                        }
+                      },
+                      darkMode: true, // default: false
                     ),
-                    transform: Matrix4.rotationZ(-8 * pi / 180)
-                      ..translate(-10.0),
-                    child: Text(
-                      'NoFSD',
-                      style: TextStyle(
-                          color:
-                              Theme.of(context).accentTextTheme.headline6.color,
-                          fontSize: 45,
-                          fontFamily: 'Anton'),
-                    ),
-                  ),
-                  AuthCard(),
-                  GoogleSignInButton(
-                    onPressed: () async {
-                      bool f;
-                      firebaseService.handleSignIn();
-                      f = await firebaseService.usuario.toString() != null;
-                      if (f) {
-                        Navigator.of(context).pushNamed(AppRotas.HOME);
-                      }
-                    },
-                    darkMode: true, // default: false
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
