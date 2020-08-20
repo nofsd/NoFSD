f9304784baefe16fb59ae481957ad42538523f3c
Bruno Soares de Brito <bsb.brunosoares@gmail.com>
2020-08-20 16:40:31 -0300

    CRUD atualizado

--- lib/models/mobile_model.dart
+++ lib/models/mobile_model.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'mobile_model.g.dart';
+
+@JsonSerializable()
 class MobileModel with ChangeNotifier {
   final String id;
   final String modelo;
@@ -14,20 +18,29 @@ class MobileModel with ChangeNotifier {
   final String numero;
   final String ddd;
   final String pais;
+  final String dataLocacao;
+  final String dataDevolucao;
+  final String status;
 
-  MobileModel({
-    this.id,
-    @required this.modelo,
-    @required this.marca,
-    @required this.imei,
-    this.vouncher,
-    this.tradeIn,
-    @required this.usuario,
-    @required this.prv,
-    @required this.operadora,
-    this.pin,
-    @required this.numero,
-    @required this.ddd,
-    this.pais,
-  });
+  MobileModel(
+      {this.id,
+      @required this.modelo,
+      @required this.marca,
+      @required this.imei,
+      this.vouncher,
+      this.tradeIn,
+      @required this.usuario,
+      @required this.prv,
+      @required this.operadora,
+      this.pin,
+      @required this.numero,
+      @required this.ddd,
+      this.pais,
+      this.dataLocacao,
+      this.dataDevolucao,
+      this.status});
+
+  factory MobileModel.fromJson(Map<String, dynamic> json) =>
+      _$MobileModelFromJson(json);
+  Map<String, dynamic> toJson() => _$MobileModelToJson(this);
 }
--- lib/provider/mobiles_provider.dart
+++ lib/provider/mobiles_provider.dart
@@ -3,11 +3,13 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:hack_infor/exceptions/http_exception.dart';
 import 'package:hack_infor/models/mobile_model.dart';
+import 'package:hack_infor/provider/networking.dart';
 import 'package:hack_infor/utils/constantes.dart';
 import 'package:http/http.dart' as http;
 
 class MobilesProvider with ChangeNotifier {
   final String _baseUrl = '${Constantes.BASE_API_URL}/mobiles';
+  final String _baseBack = '${Constantes.BASE_API_BACK}/mobiles';
   List<MobileModel> _itemsMobiles = [];
 
   String _token;
@@ -21,61 +23,18 @@ class MobilesProvider with ChangeNotifier {
   }
 
   Future<void> loadMobiles() async {
-    final response = await http.get("$_baseUrl.json?auth=$_token");
-    Map<String, dynamic> data = json.decode(response.body);
-
-    // abaixo ele vai limpar a lista de mobiles para não duplicar a lista ao ser chamada.
-    _itemsMobiles.clear();
-    if (data != null) {
-      data.forEach((mobileId, mobileData) {
-        _itemsMobiles.add(MobileModel(
-          id: mobileId,
-          modelo: mobileData['modelo'],
-          marca: mobileData['marca'],
-          imei: mobileData['imei'],
-          usuario: mobileData['usuario'],
-          prv: mobileData['prv'],
-          operadora: mobileData['operadora'],
-          ddd: mobileData['ddd'],
-          numero: mobileData['numero'],
-        ));
-      });
-      //print(json.decode(response.body));
-      notifyListeners();
-    }
+    NetworkHelper networkHelper = NetworkHelper("$_baseBack");
+    List<dynamic> data = await networkHelper.getData();
+    _itemsMobiles = data.map((e) => MobileModel.fromJson(e)).toList();
+    notifyListeners();
     return Future.value();
   }
 
-  // se alguem chama o metódo para add
   Future<void> addMobile(MobileModel newMobile) async {
-    //await substitui o then
-
-    final response = await http.post(
-      "$_baseUrl.json?auth=$_token",
-      body: json.encode({
-        'modelo': newMobile.modelo,
-        'marca': newMobile.marca,
-        'imei': newMobile.imei,
-        'usuario': newMobile.usuario,
-        'prv': newMobile.prv,
-        'operadora': newMobile.operadora,
-        'ddd': newMobile.ddd,
-        'numero': newMobile.numero,
-      }),
-    );
-
-    _itemsMobiles.add(MobileModel(
-      id: json.decode(response.body)['name'],
-      modelo: newMobile.modelo,
-      marca: newMobile.marca,
-      imei: newMobile.imei,
-      usuario: newMobile.usuario,
-      prv: newMobile.prv,
-      operadora: newMobile.operadora,
-      ddd: newMobile.ddd,
-      numero: newMobile.numero,
-    ));
-    //notifyListeners = vai notificar todos os interessados quando o evento for chamado
+    print('ADD');
+    NetworkHelper networkHelper = NetworkHelper("$_baseBack");
+    await networkHelper.postData(newMobile.toJson());
+    _itemsMobiles.add(newMobile);
     notifyListeners();
   }
 
@@ -83,41 +42,25 @@ class MobilesProvider with ChangeNotifier {
     if (mobile == null || mobile.id == null) {
       return;
     }
-    //linha abaixo encontra o aparelho a ser alterado
+
+    print('Update: $mobile.id');
+
+    NetworkHelper networkHelper = NetworkHelper("$_baseBack/$mobile.id");
+    await networkHelper.putData(mobile.toJson());
     final index = _itemsMobiles.indexWhere((prod) => prod.id == mobile.id);
-    if (index >= 0) {
-      await http.patch(
-        "$_baseUrl/${mobile.id}.json?auth=$_token",
-        body: json.encode({
-          'modelo': mobile.modelo,
-          'marca': mobile.marca,
-          'imei': mobile.imei,
-          'usuario': mobile.usuario,
-          'prv': mobile.prv,
-          'operadora': mobile.operadora,
-          'ddd': mobile.ddd,
-          'numero': mobile.numero,
-        }),
-      );
-      _itemsMobiles[index] = mobile;
-      notifyListeners();
-    }
+    _itemsMobiles[index] = mobile;
+    notifyListeners();
   }
 
   Future<void> deleteMobile(String id) async {
-    //linha abaixo encontra o aparelho a ser deletado
-    final index = _itemsMobiles.indexWhere((mobile) => mobile.id == id);
+    NetworkHelper networkHelper = NetworkHelper("$_baseBack/$id");
+    await networkHelper.deleteData();
+    final index = _itemsMobiles.indexWhere((prod) => prod.id == id);
+
     if (index >= 0) {
       final mobile = _itemsMobiles[index];
       _itemsMobiles.remove(mobile);
       notifyListeners();
-      final response =
-          await http.delete("$_baseUrl/${mobile.id}.json?auth=$_token");
-      if (response.statusCode >= 400) {
-        _itemsMobiles.insert(index, mobile);
-        notifyListeners();
-        throw HttpException('Ocorreu um erro na exclusão do aparelho.');
-      }
     }
   }
 }
